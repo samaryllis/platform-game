f294df32890e13080bd35c551eeeff720f966faa
Selen <tabletevko@gmail.com>
2021-05-08 16:14:33 +0200

    first commit

---
+++ platformer_tut1.py
@@ -0,0 +1,44 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+#define game variables
+tile_size = 50
+
+
+#load images
+sun_img = pygame.image.load('img/sun.png')
+bg_img = pygame.image.load('img/sky.png')
+
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line, * tile_size, 0), (line, * tile_size, screen_height))
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+
+        #load images
+        dirt_img = pygame.image.load
+
+run = True
+while run:
+    screen.blit(bg_img, (0, 0))
+    screen.blit(sun_img, (100, 100))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
