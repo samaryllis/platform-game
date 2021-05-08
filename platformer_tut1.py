c4156862f925359a379829da35f2bd251edc1e0b
Selen <tabletevko@gmail.com>
2021-05-08 16:23:22 +0200

    bug fix

--- platformer_tut1.py
+++ platformer_tut1.py
@@ -9,31 +9,34 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
-#define game variables
+# define game variables
 tile_size = 50
 
-
-#load images
+# load images
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
+
 
 def draw_grid():
     for line in range(0, 20):
         pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line, * tile_size, 0), (line, * tile_size, screen_height))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
 
 class World():
     def __init__(self, data):
         self.tile_list = []
 
-
-        #load images
+        # load images
         dirt_img = pygame.image.load
+
 
 run = True
 while run:
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
+
+    draw_grid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
